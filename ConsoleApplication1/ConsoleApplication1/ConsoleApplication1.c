959211294b5cd458d2b4ccce4d255271b86b1f43
futohashi <chicago_bulls_eye@hotmail.co.jp>
2017-05-12 17:07:20 +0900

    tab修正

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.c
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.c
@@ -7,8 +7,8 @@
 
 void error(char *msg)
 {
-		fprintf(stderr, "%s:%s\n", msg, strerror(errno));
-		exit(1);
+	fprintf(stderr, "%s:%s\n", msg, strerror(errno));
+	exit(1);
 }
 
 int is_Correct_ip(char *ipAddr);
@@ -17,40 +17,38 @@ int charPointToInt(char *pChar);
 
 int main(void) 
 {
-    char ipNumStr[10];
-
+	char ipNumStr[10];
+	
     fgets(ipNumStr, sizeof(ipNumStr), stdin);
     int ipNum = atoi(ipNumStr);
     if (ipNum)
     {
-      char ipAddrs[1024];
-      ipAddrs[0] = '\0';
+		char ipAddrs[1024];
+		ipAddrs[0] = '\0';
 
       for (int i = 0; i<ipNum; i++)
       {
-					char eachIpAddr[100];
-					for (int i = 0; i < 100; i++)
-						eachIpAddr[i] = '\0';
+			char eachIpAddr[100];
+			for (int i = 0; i < 100; i++){ eachIpAddr[i] = '\0'; }
+			fgets(eachIpAddr, sizeof(eachIpAddr), stdin);
+			//printf("%s", ipaddr);
 
-					fgets(eachIpAddr, sizeof(eachIpAddr), stdin);
-					//printf("%s", ipaddr);
-
-					for (int i = 0; i < 100; i++)
-					{
-						if (eachIpAddr[i] == '\n') 
-						{
-							eachIpAddr[i] = ',';
-							break;
-						}
-					}
-					
-					strcat(ipAddrs, eachIpAddr);
+			for (int i = 0; i < 100; i++)
+			{
+				if (eachIpAddr[i] == '\n') 
+				{
+					eachIpAddr[i] = ',';
+					break;
 				}
+			}
+				
+			strcat(ipAddrs, eachIpAddr);
+		}
 
     //hantei
-		printf("ipAddrs %s\n", ipAddrs);
+	printf("ipAddrs %s\n", ipAddrs);
     is_Correct_ip(ipAddrs);
-  }
+	}
 
   return 0;
 }
@@ -59,54 +57,59 @@ int main(void)
 int is_Correct_ip(char *ipAddr)
 {
     char *eachIpAddr[50];
-		int cnt = 0;
-		for(eachIpAddr[cnt] = strtok(ipAddr, ","); eachIpAddr[cnt] != NULL; eachIpAddr[++cnt] = strtok(NULL, ","))
-		{
-			printf("eachIpAddr[%i] %s\n", cnt, eachIpAddr[cnt]);
-		}
-		printf("cnt %d\n", cnt);
+	int cnt = 0;
+	for(eachIpAddr[cnt] = strtok(ipAddr, ","); eachIpAddr[cnt] != NULL; eachIpAddr[++cnt] = strtok(NULL, ","))
+	{
+		printf("eachIpAddr[%i] %s\n", cnt, eachIpAddr[cnt]);
+	}
+	
+	printf("cnt %d\n", cnt);
 
-		int correctSegNum  = 0;
-		char *segIpNum[50];
-		int cnt2 =0;
-		int i=0;
-		for(segIpNum[cnt2] = strtok(eachIpAddr[i], "."); 
-				i < cnt; 
-				segIpNum[++cnt2] = strtok(eachIpAddr[++i], "."))
-		{
-				correctSegNum =0;
-				for(; segIpNum[cnt2] != NULL; segIpNum[++cnt2] = strtok(NULL, "."))
-				{
-					correctSegNum += segmentRangeIsMatch(atoi(segIpNum[cnt2]));
-				}
-				printf("cnt2 %d\n", cnt2);
-				cnt2--;//NULL代入を消す
+	int correctSegNum  = 0;
+	char *segIpNum[50];
+	int cnt2 =0;
+	int i=0;
+	for(segIpNum[cnt2] = strtok(eachIpAddr[i], "."); 
+		i < cnt; 
+		segIpNum[++cnt2] = strtok(eachIpAddr[++i], "."))
+	{
+		correctSegNum =0;
 
-				if (correctSegNum == 4)
-				{
-					printf("true\n");
-				}
-				else
-				{
-					printf("false\n");
-				}
-				printf("\n");
+		for(; segIpNum[cnt2] != NULL; segIpNum[++cnt2] = strtok(NULL, "."))
+		{
+			correctSegNum += segmentRangeIsMatch(atoi(segIpNum[cnt2]));
 		}
 
-		for(int i=0; i < cnt2; i++)
+		printf("cnt2 %d\n", cnt2);
+		cnt2--;//NULL代入を消す
+
+		if (correctSegNum == 4)
 		{
-			//printf("segIpNum[%i] %s\n", i, segIpNum[i]);
+			printf("true\n");
 		}
-			return 0;
+		else
+		{
+			printf("false\n");
+		}
+
+		printf("\n");
+	}
+
+	for(int i=0; i < cnt2; i++)
+	{
+		//printf("segIpNum[%i] %s\n", i, segIpNum[i]);
+	}
+
+	return 0;
 }
 
 //return 0 or 1
 int segmentRangeIsMatch(int segNum) 
 {
-		int cnt;
-		printf("segNum %d\n", segNum);
-		if (segNum >= 1 && segNum <= 100)
-		{
+	int cnt;
+	printf("segNum %d\n", segNum);
+	if (segNum >= 1 && segNum <= 100)
+	{
         return 1;
     }
     return 0;
